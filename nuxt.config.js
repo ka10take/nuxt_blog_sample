af1a9241e653024f638f6690519f679dcc84b625
siropaca <ryhshr3@gmail.com>
2020-12-03 21:20:17 +0900

    フロント作成まで

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,18 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt-blog-sample',
+    titleTemplate: '%s | nuxt-blog-sample',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css'
+      }
     ]
   },
 
